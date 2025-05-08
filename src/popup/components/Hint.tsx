4fd94b1c03ab6b07af99ceadd52df9c89d495cb1
Pavel Barovsky <pbarovsky@mail.ru>
2025-05-08 10:57:04 +0300

    fix Hint

--- src/popup/components/Hint.tsx
+++ src/popup/components/Hint.tsx
@@ -1,26 +1,24 @@
 import { Tooltip } from "antd";
+import { CSSProperties } from "react";
 
 interface HintProps {
   text: string;
 }
 
+const HintStyles: CSSProperties = {
+  whiteSpace: "pre-wrap",
+  wordBreak: "break-word",
+  lineHeight: "normal",
+  fontSize: "12px",
+};
+
 export const Hint = ({ text }: HintProps) => {
-  let formattedText = text;
-  try {
-    const parsedJson = JSON.parse(text);
-    formattedText = JSON.stringify(parsedJson, null, 2);
-  } catch {
-    formattedText = text;
-  }
+  const formattedText = /^[{[]/.test(text)
+    ? JSON.stringify(JSON.parse(text), null, 2)
+    : text;
 
   return (
-    <Tooltip
-      title={
-        <pre style={{ whiteSpace: "pre-wrap", wordBreak: "break-word" }}>
-          {formattedText}
-        </pre>
-      }
-    >
+    <Tooltip title={<pre style={HintStyles}>{formattedText}</pre>}>
       {text}
     </Tooltip>
   );
