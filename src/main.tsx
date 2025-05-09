d9780ce00e411b60272d0812414f430251b527b4
Pavel Barovsky <pbarovsky@mail.ru>
2025-05-09 21:39:43 +0300

    build branch

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { App } from "./popup/app/App";
-import "@ant-design/v5-patch-for-react-19";
-
-const root = createRoot(document.getElementById("app")!);
-root.render(<App />);
